d002207e5350e9d171d1f95692408ceb6c8fdf89
jammin-ben <benjamin.reber19@houghton.edu>
2020-06-04 17:17:37 -0400

    tweaked asteroids

--- Misc/Asteroid.gd
+++ Misc/Asteroid.gd
@@ -7,7 +7,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.frame=floor(randf()*3)
+	self.rotate(randf()*2*PI)
 	print("i am metero")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position.y+=delta*2
+	self.position.y+=delta*20
+	self.rotation+=1*delta
--- Misc/Asteroid_Spawner.gd
+++ Misc/Asteroid_Spawner.gd
@@ -11,5 +11,8 @@ func _ready():
 	
 	for i in range(10):
 		var asteroid_instance=asteroid.instance()
-		#self.add_child(asteroid_instance)
-		get_tree().root.add_child(asteroid_instance)
+		self.add_child(asteroid_instance)
+		#get_tree().root.add_child(asteroid_instance)
+		asteroid_instance.position.x += randf()*200 - 100
+		
+		asteroid_instance.position.y += randf()*200 - 100
